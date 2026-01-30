2ec60eb955954f64bd09f95cbd9ec965e5fd57a0
shabab11715 <shababaffanul@gmail.com>
2026-01-30 14:33:40 +0600

    Mod program.cs

--- Program.cs
+++ Program.cs
@@ -19,14 +19,14 @@ int GCD(int a, int b)
 app.MapGet("/shababaffanul_gmail_com", (string? x, string? y) =>
 {
     if(!int.TryParse(x, out int x_nat) || !int.TryParse(y,out int y_nat))
-        return "NaN";
+        return Results.Text("NaN", "text/plain");
     if(x_nat <= 0 || y_nat <= 0)
-        return "NaN";
+        return Results.Text("NaN", "text/plain");
     
     int gcd = GCD(x_nat, y_nat);
     int lcm = (x_nat / gcd) * y_nat;
 
-    return lcm.ToString();
+    return Results.Text(lcm.ToString(), "text/plain");
 });
 
 app.Run();
