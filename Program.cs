37461989f5dd87b88f13863118864ff2f7219c03
shabab11715 <shababaffanul@gmail.com>
2026-01-30 12:58:44 +0600

    add all

---
+++ Program.cs
@@ -0,0 +1,32 @@
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.Hosting;
+
+var builder = WebApplication.CreateBuilder(args);
+var app = builder.Build();
+
+int GCD(int a, int b)
+{
+    while(b!= 0)
+    {
+        int temp = b;
+        b = a % b;
+        a = temp;
+    }
+
+    return a;
+}
+
+app.MapGet("/shababaffanul_gmail_com", (string? x, string? y) =>
+{
+    if(!int.TryParse(x, out int x_nat) || !int.TryParse(y,out int y_nat))
+        return "NaN";
+    if(x_nat <= 0 || y_nat <= 0)
+        return "NaN";
+    
+    int gcd = GCD(x_nat, y_nat);
+    int lcm = (x_nat / gcd) * y_nat;
+
+    return lcm.ToString();
+});
+
+app.Run();
